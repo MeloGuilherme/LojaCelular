6976c3346768463d9cf59470b74787d1a5400339
MeloGuilherme <guilhermeamelo.dev@gmail.com>
2019-11-17 23:44:41 -0300

    Atualizando layout das telas - V_1.0.0

--- src/br/unitins/lojacelular/controller/LoginController.java
+++ src/br/unitins/lojacelular/controller/LoginController.java
@@ -14,26 +14,48 @@ public class LoginController {
 	private Usuario usuario;
 
 	public void logar() {
-		
+
 		UsuarioDAO dao = new UsuarioDAO();
-		
+
 		String hashSenha = Util.gerarHash(getUsuario().getSenha());
-		
+
 		Usuario usuario = dao.login(getUsuario().getLogin(), hashSenha);
-		
+
 		if (usuario != null) {
-			
+
 			// armazenando um usuario na sessao
 			Session.getInstance().setAttribute("usuarioLogado", usuario);
 			Util.redirect("menu.xhtml");
+			Integer num = usuario.getPerfil().getValue();
+
+			if (num.equals(1)) {
+
+				System.out.println("O usuário é um Administrador.");
+			}
+
+			else if (num.equals(2)) {
+
+				System.out.println("O usuário é um Funcionário.");
+			}
+
+			else if (num.equals(3)) {
+
+				System.out.println("O usuário é um Cliente.");
+
+			}
+
+			else {
+
+				System.out.println("Falha na autenticação.");
+			}
 		}
-		
+
 		else {
-			
+
 			Util.addMessageError("Usuário ou Senha Inválido.");
 		}
 	}
-	
+
 	public void limpar() {
 		setUsuario(new Usuario());
 //		usuario = new Usuario();
